069def983191f229eaca8d97815fd3138d64ec28
Matikuper <167240569+Matikuper@users.noreply.github.com>
2025-05-07 15:43:44 -0300

    Suma y Resultado terminado
    ACADE

---
+++ Assets/Scripts/Script.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Script : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    public TMP_InputField input1;
+    public TMP_InputField input2;
+    public TextMeshProUGUI textoResultado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textoResultado.text = string.Empty;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void sumarYMostrarResultado()
+    {
+        //tomar los valores del input y sumarlos
+        num1 = int.Parse(input1.text);
+        num2 = int.Parse(input2.text);
+
+        textoResultado.text = $"{num1 + num2}";
+    }
+
+}
